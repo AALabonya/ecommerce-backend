b2627f2addc9edbd57507f76c3278fde80df2c7e
AALabonya <1aklimakter@gmail.com>
2024-05-21 22:02:51 -0700

    first commit

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import app from "./app";
+
+async function main() {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://ecommerceBackend:3jbgOonMqdOFarfd@cluster0.waps95s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    );
+  } catch (err) {
+    console.log(err);
+  }
+}
+main();
+
+app.listen(5000, () => {
+  console.log(`Example app listening on port ${5000}`);
+});
