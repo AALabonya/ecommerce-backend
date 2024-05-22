8841af8902031cdfd0c3a8172469371c2d2bb62d
AALabonya <1aklimakter@gmail.com>
2024-05-22 08:15:04 -0700

    added README.md file

--- src/app.ts
+++ src/app.ts
@@ -29,11 +29,11 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 // not found route
-app.get('*', (req: Request, res: Response) => {
-    res.status(404).json({
-        success: false,
-        message: 'Route not found',
-    });
-});
+// app.get('*', (req: Request, res: Response) => {
+//     res.status(404).json({
+//         success: false,
+//         message: 'Route not found',
+//     });
+// });
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,9 @@
 import mongoose from 'mongoose';
 import app from './app';
-import { Server } from 'http';
+
 import config from './app/config';
 
-let server: Server;
 async function main() {
-    const a = 10;
     try {
         await mongoose.connect(config.database_url as string);
         console.log('MongoDB Database Connected Successfully');
